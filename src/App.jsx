285bde522a7fd8858cf765e9fabbce7a9528c92e
Shahriar Alom Fardin <mdfardin496@gmail.com>
2024-11-19 02:50:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="text-red-700 font-extrabold text-3xl">App</div>
+    </>
+  );
+};
+
+export default App;
