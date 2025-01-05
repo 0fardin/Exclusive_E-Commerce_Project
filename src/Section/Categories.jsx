acb9f0cc817d389aae34ad3bc8cbeb64e11aafb1
Shahriar Alom Fardin <mdfardin496@gmail.com>
2025-01-06 02:05:05 +0600

    categories

--- src/Section/Categories.jsx
+++ src/Section/Categories.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { FaArrowRight } from "react-icons/fa6";
 import { FaArrowLeft } from "react-icons/fa6";
+import { CiMobile3 } from "react-icons/ci";
+import { RiComputerLine } from "react-icons/ri";
+import { BsSmartwatch } from "react-icons/bs";
+import { IoCameraOutline } from "react-icons/io5";
+import { FiHeadphones } from "react-icons/fi";
+import { IoGameControllerOutline } from "react-icons/io5";
 
 const Categories = () => {
   return (
@@ -24,6 +30,44 @@ const Categories = () => {
               <div className="w-7 h-7 rounded-full bg-deepsecondary text-primary flex items-center justify-center">
                 <FaArrowRight />
               </div>
+            </div>
+          </div>
+          <div className=" mt-16 flex gap-8">
+            <div className=" w-[170px] h-[145px] border border-primary/50 rounded-md flex flex-col items-center justify-center group hover:bg-third hover:border-third duration-300 ">
+              <CiMobile3 className=" text-6xl  text-primary group-hover:text-white" />
+              <p className=" text-[16px] leading-6 text-primary font-normal group-hover:text-white">
+                Phones
+              </p>
+            </div>
+            <div className=" w-[170px] h-[145px] border border-primary/50 rounded-md flex flex-col items-center justify-center group hover:bg-third hover:border-third duration-300">
+              <RiComputerLine className=" text-6xl  text-primary group-hover:text-white" />
+              <p className=" text-[16px] leading-6 text-primary font-normal group-hover:text-white">
+                Computers
+              </p>
+            </div>
+            <div className=" w-[170px] h-[145px] border border-primary/50 rounded-md flex flex-col items-center justify-center group hover:bg-third hover:border-third duration-300">
+              <BsSmartwatch className=" text-6xl  text-primary group-hover:text-white" />
+              <p className=" text-[16px] leading-6 text-primary font-normal group-hover:text-white">
+                SmartWatch
+              </p>
+            </div>
+            <div className=" w-[170px] h-[145px] border border-primary/50 rounded-md flex flex-col items-center justify-center group hover:bg-third hover:border-third duration-300">
+              <IoCameraOutline className=" text-6xl  text-primary group-hover:text-white" />
+              <p className=" text-[16px] leading-6 text-primary font-normal group-hover:text-white">
+                Camera
+              </p>
+            </div>
+            <div className=" w-[170px] h-[145px] border border-primary/50 rounded-md flex flex-col items-center justify-center group hover:bg-third hover:border-third duration-300">
+              <FiHeadphones className=" text-6xl  text-primary group-hover:text-white" />
+              <p className=" text-[16px] leading-6 text-primary font-normal group-hover:text-white">
+                HeadPhones
+              </p>
+            </div>
+            <div className=" w-[170px] h-[145px] border border-primary/50 rounded-md flex flex-col items-center justify-center group hover:bg-third hover:border-third duration-300">
+              <IoGameControllerOutline className=" text-6xl  text-primary group-hover:text-white" />
+              <p className=" text-[16px] leading-6 text-primary font-normal group-hover:text-white">
+                Gaming
+              </p>
             </div>
           </div>
         </div>
