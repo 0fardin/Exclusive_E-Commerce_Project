6b98dbc0a2680353110dd174828091a78f892b8b
Shahriar Alom Fardin <mdfardin496@gmail.com>
2025-02-19 12:02:13 +0600

    footer last update

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Banner from "./Section/Banner";
 import FeaturesProduct from "./Section/FeaturesProduct";
 import Categories from "./Section/Categories";
 import Footer from "./Section/Footer";
+import Footerlast from "./Section/Footerlast";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <FeaturesProduct />
       <Categories />
       <Footer />
+      <Footerlast />
     </>
   );
 };
---
+++ src/Section/Footerlast.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footerlast = () => {
+  return (
+    <>
+      <div className="bg-primary border-t-[1px] border-secondary/10">
+        <div className="container w-container">
+          <div className="py-6 text-sm text-center text-stone-500">
+            © 1968 Company Co. All rights reserved.
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footerlast;
